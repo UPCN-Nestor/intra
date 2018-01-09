bb596bc03280f9b641808090d0a63a37597f87ab
Nestor Echegoyen <nestor.echegoyen@upcnecochea.com.ar>
2018-01-09 12:14:38 -0300

    "

--- php/userspice/tal_tareas_w.php
+++ php/userspice/tal_tareas_w.php
@@ -3,5 +3,20 @@
 	require('check_permissions.php');
 
 	$tabla = "tal_tareas";
+	
+	// *** Si se está insertando desde formulario, se calcula la orden de trabajo (se permite editarla manualmente desde la grilla)	
+	if(!isset($_POST["id_orden_trabajo"])) {		
+		require_once 'pdo_mysql.php';
+		$pdo->query("SET NAMES 'utf8'");
+		
+		$f = $_POST['fecha_creacion'];
+		$v = $_POST['id_vehiculo'];
+		$sql="SELECT ID FROM TAL_ORDENES_TRABAJO WHERE FECHA < '$f' AND ID_VEHICULO = '$v' ORDER BY FECHA DESC";
+		
+		try{ $q = $pdo->query($sql)->fetch()['ID']; $_POST["id_orden_trabajo"] = $q; }
+		catch(PDOException $e) { echo '["error"]';	}
+	}		
+	// ***
+	
 	include('abm.php');
 ?>
